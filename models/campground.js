9a6c9b66f42bd9b092f4b42852a44e62856d0fcf
D Marcus <mindlesscorporatedrone@gmail.com>
2021-01-30 13:52:25 -0800

    added reviews & added it to the campground schema

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -6,7 +7,26 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            //acquire review ID & populate using Review model
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
+
+//review needs to be deleted (review is linked to campground) after the corresponding campground has been deleted through middleware (after every findOneAndDelete)
+//previously deleted item is passed to middleware function
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+} )
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
