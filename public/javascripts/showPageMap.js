391788a85347de99656287ec482097ee2e4bfb3b
D Marcus <mindlesscorporatedrone@gmail.com>
2021-02-04 14:38:48 -0800

    added basic security & fixed show map issue

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,13 +1,14 @@
 mapboxgl.accessToken = mapToken;
+console.log(campground.geometry.coordinates);
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-74.5, 40], // starting position [lng, lat] campground.geometry.coordinates
+    center: campground.geometry.coordinates, // starting position [lng, lat] campground.geometry.coordinates
     zoom: 10 // starting zoom
 });
 
 new mapboxgl.Marker()
-    .setLngLat([-74.5, 40]) //campground.geometry.coordinates
+    .setLngLat(campground.geometry.coordinates) //campground.geometry.coordinates
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
             .setHTML(
